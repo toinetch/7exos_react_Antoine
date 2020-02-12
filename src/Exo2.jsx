3cf0fb6ae4d7176434ac4e3ffce8cb3c9d7691a2
Antoine <antoine.gouders@hotmail.com>
2020-02-12 14:47:35 +0100

    Exercice 2

---
+++ src/Exo2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exo2 = () => {
+    return(
+        <div>
+            <p>Mettre un écouteur d'evenements sur les boutons nav et changer le panel en consequence</p>
+        </div>
+    );
+}
+
+export default Exo2;
